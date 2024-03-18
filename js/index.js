54abc51c9585933597aff894267ab37b345ec8d3
Sharon Wong <sharonwong@sharons-mbp.attlocal.net>
2024-03-17 21:51:42 -0700

    Update navbar and structure questions page

--- js/index.js
+++ js/index.js
@@ -36,3 +36,12 @@ window.addEventListener('pageshow', function (event) {
     var fader = document.getElementById('fader');
     fader.classList.remove('fade-in');
 });
+
+$(document).ready(function(){ 
+    $(".tab-a").click(function(){  
+        $(".tab").removeClass("tab-active");
+        $(".tab[data-id='" + $(this).attr('data-id') + "']").addClass("tab-active");
+        $(".tab-a").removeClass("active-a");
+        $(this).addClass("active-a");
+    });
+});
